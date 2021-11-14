14b70da7865d3c8244e9d2ec7bcc4b24ddf7c0aa
Carl <74821542+sk4rif@users.noreply.github.com>
2021-11-14 20:53:28 +0000

    Create threat_detector.js

---
+++ public/threat_detector.js
@@ -0,0 +1,168 @@
+
+<style>html body {overflow: hidden}
+.camera{
+    position: absolute;
+    top: 40px;
+}
+</style>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       
+      
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Carl Grimaldi Chart</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!--Loader -->
+      <link rel="stylesheet" href="css/loader.css">
+     
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+        <script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.min.js"></script>
+        <script src="https://unpkg.com/ml5@0.5.0/dist/ml5.min.js"></script>
+
+        <meta chartset="UTF-8">
+        <meta name="viewport" content="width=device-width" , initial-scale=1.0>
+        <meta http-equiv="X_UA-Compatible" content="ie=edge">
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="/socket.io/socket.io.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.2/chart.min.js"></script>
+        </link>
+    
+   </head>
+
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/carl_g.gif" alt="#" /></div>
+      </div>
+
+      
+      <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+      <div class="popup center ">
+        <div class="icon">
+          <i class="fa fa-check"></i>
+        </div>
+        <div class="title">
+            Syncronised Graph
+        </div>
+        <div class="description">
+            What you are seeing is a  graph that simulated stock market mouvements. It is syncronysed with all viewers of this website using socket.io (webRTC). You can also bet on the stock. Have fun!
+        </div>
+        <div class="dismiss-btn">
+          <button id="dismiss-popup-btn">
+            Dismiss
+          </button>
+        </div>
+       </div>
+       <div class="center open-btn">
+          <button id="open-popup-btn">
+            About Cam
+          </button>
+      </div>
+
+        <nav id="nav-1">
+
+            <a class="link-1" href="index.html">Me</a>
+            <a class="link-1" href="play.html">SpaceMan2D</a>
+            <a class="link-1" href="finance.html">Sync. Chart</a>
+            <a class="link-1" href="python.html">Python Stuff</a>
+            <a class="link-1" href="threat_detector.html">A.I detector</a>
+    
+            <link rel="stylesheet" href="css/navbar-play.css">
+        </nav>
+   
+        <div id = camera>
+            <script>
+
+                // let img;
+                let video;
+                let detector;
+                let detections = [];
+                
+                function preload() {
+                  // img = loadImage('dog_cat.jpg');
+                  detector = ml5.objectDetector('cocossd');
+                }
+                
+                function gotDetections(error, results) {
+                  if (error) {
+                    console.error(error);
+                  }
+                  detections = results;
+                  detector.detect(video, gotDetections);
+                }
+                
+                function setup() {
+                  createCanvas(640, 480);
+                  video = createCapture(VIDEO);
+                  video.size(640, 480);
+                  video.hide();
+                  detector.detect(video, gotDetections);
+                }
+                
+                let r = 0
+                let g = 255
+                let b = 0
+                function draw() {
+                  image(video, 0, 0);
+                  for (let i = 0; i < detections.length; i++) {
+                    let object = detections[i];
+                    stroke(r, g, b);
+                    strokeWeight(4);
+                    noFill();
+                    rect(object.x, object.y, object.width, object.height);
+                    noStroke();
+                    fill(255);
+                    textSize(24);
+                    text(object.label, object.x + 10, object.y + 24);
+                    if (object.label=='person'){
+                        r = 255,
+                        g = 0,
+                        b = 0,
+                        console.log('person');
+
+                    }
+                    else {
+                        r =0
+                        g =  255
+                        b =0 
+                    }
+                  }
+                
+                  
+                }
+                
+                
+                </script>
+            
+        </div>
+
+        <script src="js/jquery.min.js"></script>
+
+        <script src="js/design.js"></script>
+        
+    
+        <!-- sidebar -->
+
+        <script src="js/custom.js"></script>
+
+   </body>
+</html>
+
+
+
+
